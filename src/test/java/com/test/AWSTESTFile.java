57ad3e2b2e1a569f170dbeadb0ad0c2b3c9eed6a
Ajay <bori1.sotu@gmail.com>
2024-05-08 14:48:49 +0530

    First Commit

---
+++ src/test/java/com/test/AWSTESTFile.java
@@ -0,0 +1,33 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class AWSTESTFile {
+	
+	WebDriver driver;
+	
+	
+	@BeforeTest
+	public void setUp() throws InterruptedException {
+		
+		
+	}
+	
+	@Test
+	public void fileUpload() {
+		
+		System.out.println("-------> Running Test");
+		System.out.println("-------> Pass");
+		
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		
+	}
+
+}
